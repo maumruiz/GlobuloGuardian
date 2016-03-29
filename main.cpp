6b3d3ace771dddc3fd58cde8c6603f6e5f25a646
jorgelmarquez94 <jorgelmarquez94@gmail.com>
2016-03-29 16:06:34 -0600

    Cambio de imagenes
    Uso de ruta fisica

--- main.cpp
+++ main.cpp
@@ -10,17 +10,131 @@
 #include <stdio.h>
 #include <iostream>
 #include <time.h>
+#include "imageloader.h"
 
 using namespace std;
 
+//__FILE__ is a preprocessor macro that expands to full path to the current file.
+string fullPath = __FILE__;
+using namespace std;
+int angulo=0;
+static GLuint texName[36];
+const int TEXTURE_COUNT=7;
+int z=1;
+
+//le borramos el exceso para solo obtener el Path padre
+void getParentPath()
+{
+    for (int i = (int)fullPath.length()-1; i>=0 && fullPath[i] != '\\'; i--) {
+        fullPath.erase(i,1);
+    }
+}
+
 void myTimer(int i) {
+    angulo = angulo +1;
+    if (angulo > TEXTURE_COUNT) angulo =0;
     glutPostRedisplay();
-    glutTimerFunc(100,myTimer,1);
+    glutTimerFunc(1000,myTimer,0);
+}
+
+//Makes the image into a texture, and returns the id of the texture
+void loadTexture(Image* image,int k)
+{
+
+    glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    //Filtros de ampliacion y redución con cálculo mas cercano no es tan bueno pero es rápido
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+
+    //Filtros de ampliacion y redución con cálculo lineal es mejo pero son más calculos
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+    //Map the image to the texture
+    glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
+                 0,                            //0 for now
+                 GL_RGB,                       //Format OpenGL uses for image
+                 image->width, image->height,  //Width and height
+                 0,                            //The border of the image
+                 GL_RGB, //GL_RGB, because pixels are stored in RGB format
+                 GL_UNSIGNED_BYTE, //GL_UNSIGNED_BYTE, because pixels are stored
+                 //as unsigned numbers
+                 image->pixels);               //The actual pixel data
+}
+
+void initRendering()
+{
+    //Declaración del objeto Image
+    Image* image;
+    GLuint i=0;
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    glGenTextures(TEXTURE_COUNT, texName); //Make room for our texture
+
+
+    char  ruta[200];
+    sprintf(ruta,"%s%s", fullPath.c_str() , "Texturas/Inicio.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+
+    sprintf(ruta,"%s%s", fullPath.c_str() , "Texturas/Historia.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+
+    sprintf(ruta,"%s%s", fullPath.c_str() , "Texturas/Nivel1.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+
+    sprintf(ruta,"%s%s", fullPath.c_str() , "Texturas/Inicio.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+
+    sprintf(ruta,"%s%s", fullPath.c_str() , "Texturas/Historia.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+
+    sprintf(ruta,"%s%s", fullPath.c_str() , "Texturas/Nivel1.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+
+
+    delete image;
+}
+
+void handleResize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(69.0, (float)w / (float)h, 1.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0,15 , 0, 0, 0, 0, 1, 0);
 }
 
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //Habilitar el uso de texturas
+    glEnable(GL_TEXTURE_2D);
+
+    //Elegir la textura del Quads: angulo cambia con el timer
+    glBindTexture(GL_TEXTURE_2D, texName[angulo]);
+
+    glBegin(GL_QUADS);
+    //Asignar la coordenada de textura 0,0 al vertice
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-10.0f, -10.0f, 0);
+     //Asignar la coordenada de textura 1,0 al vertice
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(10.0f, -10.0f, 0);
+     //Asignar la coordenada de textura 1,1 al vertice
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(10.0f, 10.0f, 0);
+     //Asignar la coordenada de textura 0,1 al vertice
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-10.0f, 10.0f, 0);
+    glEnd();
 
     //Globulo
     glPushMatrix();
@@ -118,19 +232,227 @@ int main(int argc, char** argv)
 {
     //Initialize GLUT
     glutInit(&argc,argv);
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     //Window
     glutInitWindowSize(1000,500);
     glutInitWindowPosition(200,100);
+    //Obtener el path de los archivos
+    getParentPath();
     glutCreateWindow("Globulo Guardian");
     //States y callbacks
-    init();
-    glutReshapeFunc(reshape); //Funcion reshape
+    initRendering();
+    glutReshapeFunc(handleResize); //Funcion reshape
     glutDisplayFunc(display); //Dibujo
     glutMouseFunc(myMouse); //Funciones del mouse
     glutKeyboardFunc(myKeyboard); //Funciones del teclado
     glutSpecialFunc(mySpecialKeyboard); //Teclas especiales
-    glutTimerFunc(100,myTimer,1); //Timer
+    glutTimerFunc(1000,myTimer,0); //Timer
 
     glutMainLoop();
 }
+
+#include <assert.h>
+#include <fstream>
+
+#include "imageloader.h"
+
+using namespace std;
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h)
+{
+
+}
+
+Image::~Image()
+{
+    delete[] pixels;
+}
+
+namespace
+{
+    //Converts a four-character array to an integer, using little-endian form
+    int toInt(const char* bytes)
+    {
+        return (int)(((unsigned char)bytes[3] << 24) |
+                     ((unsigned char)bytes[2] << 16) |
+                     ((unsigned char)bytes[1] << 8) |
+                     (unsigned char)bytes[0]);
+    }
+
+    //Converts a two-character array to a short, using little-endian form
+    short toShort(const char* bytes)
+    {
+        return (short)(((unsigned char)bytes[1] << 8) |
+                       (unsigned char)bytes[0]);
+    }
+
+    //Reads the next four bytes as an integer, using little-endian form
+    int readInt(ifstream &input)
+    {
+        char buffer[4];
+        input.read(buffer, 4);
+        return toInt(buffer);
+    }
+
+    //Reads the next two bytes as a short, using little-endian form
+    short readShort(ifstream &input)
+    {
+        char buffer[2];
+        input.read(buffer, 2);
+        return toShort(buffer);
+    }
+
+    //Just like auto_ptr, but for arrays
+    template<class T>
+    class auto_array
+    {
+    private:
+        T* array;
+        mutable bool isReleased;
+    public:
+        explicit auto_array(T* array_ = NULL) :
+        array(array_), isReleased(false)
+        {
+        }
+
+        auto_array(const auto_array<T> &aarray)
+        {
+            array = aarray.array;
+            isReleased = aarray.isReleased;
+            aarray.isReleased = true;
+        }
+
+        ~auto_array()
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+        }
+
+        T* get() const
+        {
+            return array;
+        }
+
+        T &operator*() const
+        {
+            return *array;
+        }
+
+        void operator=(const auto_array<T> &aarray)
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+            array = aarray.array;
+            isReleased = aarray.isReleased;
+            aarray.isReleased = true;
+        }
+
+        T* operator->() const
+        {
+            return array;
+        }
+
+        T* release()
+        {
+            isReleased = true;
+            return array;
+        }
+
+        void reset(T* array_ = NULL)
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+            array = array_;
+        }
+
+        T* operator+(int i)
+        {
+            return array + i;
+        }
+
+        T &operator[](int i)
+        {
+            return array[i];
+        }
+    };
+}
+
+Image* loadBMP(const char* filename)
+{
+    ifstream input;
+    input.open(filename, ifstream::binary);
+    assert(!input.fail() || !"Could not find file");
+    char buffer[2];
+    input.read(buffer, 2);
+    assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
+    input.ignore(8);
+    int dataOffset = readInt(input);
+
+    //Read the header
+    int headerSize = readInt(input);
+    int width;
+    int height;
+    switch (headerSize)
+    {
+        case 40:
+            //V3
+            width = readInt(input);
+            height = readInt(input);
+            input.ignore(2);
+            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+            assert(readShort(input) == 0 || !"Image is compressed");
+            break;
+        case 12:
+            //OS/2 V1
+            width = readShort(input);
+            height = readShort(input);
+            input.ignore(2);
+            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+            break;
+        case 64:
+            //OS/2 V2
+            assert(!"Can't load OS/2 V2 bitmaps");
+            break;
+        case 108:
+            //Windows V4
+            assert(!"Can't load Windows V4 bitmaps");
+            break;
+        case 124:
+            //Windows V5
+            assert(!"Can't load Windows V5 bitmaps");
+            break;
+        default:
+            assert(!"Unknown bitmap format");
+    }
+
+    //Read the data
+    int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+    int size = bytesPerRow * height;
+    auto_array<char> pixels(new char[size]);
+    input.seekg(dataOffset, ios_base::beg);
+    input.read(pixels.get(), size);
+
+    //Get the data into the right format
+    auto_array<char> pixels2(new char[width * height * 3]);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                pixels2[3 * (width * y + x) + c] =
+                pixels[bytesPerRow * y + 3 * x + (2 - c)];
+            }
+        }
+    }
+
+    input.close();
+    return new Image(pixels2.release(), width, height);
+}
+
